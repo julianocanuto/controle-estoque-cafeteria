c0f3b02ae50b948e46689deb726420928aea51ed
Juliano Canuto <julianocanuto@gmail.com>
2021-12-28 15:24:16 -0300

    Cria entidade Ingrediente

---
+++ src/main/java/com/julianocanuto/controleestoque/entidades/Ingrediente.java
@@ -0,0 +1,88 @@
+package com.julianocanuto.controleestoque.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_ingrediente")
+public class Ingrediente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String unidadeDeMedida;
+	private Double precoUnitario;
+
+	public Ingrediente() {
+
+	}
+
+	public Ingrediente(Long id, String nome, String unidadeDeMedida, Double precoUnitario) {
+		this.id = id;
+		this.nome = nome;
+		this.unidadeDeMedida = unidadeDeMedida;
+		this.precoUnitario = precoUnitario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUnidadeDeMedida() {
+		return unidadeDeMedida;
+	}
+
+	public void setUnidadeDeMedida(String unidadeDeMedida) {
+		this.unidadeDeMedida = unidadeDeMedida;
+	}
+
+	public Double getPrecoUnitario() {
+		return precoUnitario;
+	}
+
+	public void setPrecoUnitario(Double precoUnitario) {
+		this.precoUnitario = precoUnitario;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ingrediente other = (Ingrediente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
