20c7a91c15b522388b087f47f95cb666fe6839cf
Juliano Canuto <julianocanuto@gmail.com>
2021-12-28 14:23:24 -0300

    Adiciona projeto do spring initializr

---
+++ src/test/java/com/julianocanuto/controleestoque/ControleestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianocanuto.controleestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
