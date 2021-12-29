e9c23e2d6f8afde8121964d101a5b39e6990e729
Juliano Canuto <julianocanuto@gmail.com>
2021-12-29 15:17:02 -0300

    Cria UserDTO

---
+++ src/main/java/com/julianocanuto/controleestoque/dto/UserDTO.java
@@ -0,0 +1,83 @@
+package com.julianocanuto.controleestoque.dto;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import com.julianocanuto.controleestoque.entidades.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+
+	@NotBlank(message = "Campo obrigatório")
+	private String name;
+
+	@Email(message = "Favor entrar com email válido")
+	private String email;
+
+	private String password;
+	Set<RoleDTO> roles = new HashSet<>();
+
+	public UserDTO() {
+
+	}
+
+	public UserDTO(Long id, String name, String email, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
+
+	public UserDTO(User entity) {
+		id = entity.getId();
+		name = entity.getName();
+		email = entity.getEmail();
+		password = entity.getPassword();
+
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<RoleDTO> getRoles() {
+		return roles;
+	}
+
+}
