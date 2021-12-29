cba5d34cba574f10e2a6a275e6d0ddb8a9745a4c
Juliano Canuto <julianocanuto@gmail.com>
2021-12-29 10:06:29 -0300

    Implementa update e delete no ProdutoResource

--- src/main/java/com/julianocanuto/controleestoque/resources/ProdutoResource.java
+++ src/main/java/com/julianocanuto/controleestoque/resources/ProdutoResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +42,19 @@ public class ProdutoResource {
 		produtoDTO = service.insert(produtoDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produtoDTO.getId()).toUri();
 		return ResponseEntity.created(uri).body(produtoDTO);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProdutoDTO> update(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+		produtoDTO = service.update(id, produtoDTO);
+
+		return ResponseEntity.ok().body(produtoDTO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
 	}
 }
