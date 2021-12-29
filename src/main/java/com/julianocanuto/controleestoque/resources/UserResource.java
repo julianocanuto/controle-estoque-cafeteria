f3efe68429d8cf91bc8f2fe44115118ffc6c34d3
Juliano Canuto <julianocanuto@gmail.com>
2021-12-29 15:17:52 -0300

    Implementa CRUD do User usando UserDTO

--- src/main/java/com/julianocanuto/controleestoque/resources/UserResource.java
+++ src/main/java/com/julianocanuto/controleestoque/resources/UserResource.java
@@ -1,15 +1,26 @@
 package com.julianocanuto.controleestoque.resources;
 
-import java.util.List;
+import java.net.URI;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.julianocanuto.controleestoque.entidades.User;
+import com.julianocanuto.controleestoque.dto.UserDTO;
 import com.julianocanuto.controleestoque.services.UserService;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -19,8 +30,34 @@ public class UserResource {
 	private UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list = service.findAll();
+	public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable) {
+		Page<UserDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		UserDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserDTO dto) {
+		UserDTO newDTO = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
 	}
 }
--- src/main/java/com/julianocanuto/controleestoque/services/UserService.java
+++ src/main/java/com/julianocanuto/controleestoque/services/UserService.java
@@ -1,23 +1,84 @@
 package com.julianocanuto.controleestoque.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.julianocanuto.controleestoque.dto.RoleDTO;
+import com.julianocanuto.controleestoque.dto.UserDTO;
+import com.julianocanuto.controleestoque.entidades.Role;
 import com.julianocanuto.controleestoque.entidades.User;
+import com.julianocanuto.controleestoque.repositorios.RoleRepository;
 import com.julianocanuto.controleestoque.repositorios.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository repository;
-	
+
+	@Autowired
+	private RoleRepository roleRepository;
+
 	@Transactional(readOnly = true)
-	public List<User> findAll(){
-		return repository.findAll();
+	public List<UserDTO> findAll() {
+		List<User> list = repository.findAll();
+		return list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+
+	}
+
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable) {
+		Page<User> list = repository.findAll(pageable);
+		return list.map(user -> new UserDTO(user));
+	}
+
+	@Transactional(readOnly = true)
+	public UserDTO findById(Long id) {
+
+		Optional<User> obj = repository.findById(id);
+		User entity = obj.get();
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
+	@Transactional
+	public UserDTO update(Long id, UserDTO dto) {
+		User entity = repository.getById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+
+	}
+
+	private void copyDtoToEntity(UserDTO dto, User entity) {
+		entity.setName(dto.getName());
+		entity.setEmail(dto.getEmail());
+		entity.setPassword(dto.getPassword());
+		
+		entity.getRoles().clear();
+		for (RoleDTO roleDTO : dto.getRoles()) {
+			Role role = roleRepository.getById(roleDTO.getId());
+			entity.getRoles().add(role);
+		}
 	}
 
 }
