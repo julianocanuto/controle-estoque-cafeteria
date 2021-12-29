b6ade0a62acea8bc7f2b62fb18b86a2a7f8f3c74
Juliano Canuto <julianocanuto@gmail.com>
2021-12-29 15:40:18 -0300

    Implementa uso do DTO e retorno paginado para entidade Role

--- src/main/java/com/julianocanuto/controleestoque/resources/RoleResource.java
+++ src/main/java/com/julianocanuto/controleestoque/resources/RoleResource.java
@@ -1,14 +1,14 @@
 package com.julianocanuto.controleestoque.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.julianocanuto.controleestoque.entidades.Role;
+import com.julianocanuto.controleestoque.dto.RoleDTO;
 import com.julianocanuto.controleestoque.services.RoleService;
 
 @RestController
@@ -19,8 +19,8 @@ public class RoleResource {
 	private RoleService service;
 
 	@GetMapping
-	public ResponseEntity<List<Role>> findAll(){
-		List<Role> list = service.findAll();
+	public ResponseEntity<Page<RoleDTO>> findAllPaged(Pageable pageable){
+		Page<RoleDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/julianocanuto/controleestoque/services/RoleService.java
+++ src/main/java/com/julianocanuto/controleestoque/services/RoleService.java
@@ -1,11 +1,12 @@
 package com.julianocanuto.controleestoque.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.julianocanuto.controleestoque.dto.RoleDTO;
 import com.julianocanuto.controleestoque.entidades.Role;
 import com.julianocanuto.controleestoque.repositorios.RoleRepository;
 
@@ -16,8 +17,9 @@ public class RoleService {
 	RoleRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Role> findAll(){
-		return repository.findAll();
+	public Page<RoleDTO> findAllPaged(Pageable pageable){
+		Page<Role> list = repository.findAll(pageable);
+		return list.map(role -> new RoleDTO(role));
 	}
 
 }
