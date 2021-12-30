1ccbdb9d4bc5c818fe2b9315cfad70d74e183d25
Juliano Canuto <julianocanuto@gmail.com>
2021-12-30 16:25:41 -0300

    Adiciona id a projection ProdutoNomeQuantidade

--- src/main/java/com/julianocanuto/controleestoque/repositorios/projections/ProdutoNomeQuantidadeProjection.java
+++ src/main/java/com/julianocanuto/controleestoque/repositorios/projections/ProdutoNomeQuantidadeProjection.java
@@ -2,6 +2,7 @@ package com.julianocanuto.controleestoque.repositorios.projections;
 
 public interface ProdutoNomeQuantidadeProjection {
 
+	Long getId();
 	String getNome();
 	Integer getQuantidade();
 }
