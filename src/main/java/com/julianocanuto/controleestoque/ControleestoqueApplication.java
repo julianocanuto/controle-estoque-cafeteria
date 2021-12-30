87e7e34adb02ddf06a1d4b96601959aeab390f88
Juliano Canuto <julianocanuto@gmail.com>
2021-12-30 13:54:17 -0300

    Implementa CommandLineRunner para testar ProdutoNomeQuantidadeProjection

--- src/main/java/com/julianocanuto/controleestoque/ControleestoqueApplication.java
+++ src/main/java/com/julianocanuto/controleestoque/ControleestoqueApplication.java
@@ -1,13 +1,37 @@
 package com.julianocanuto.controleestoque;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.julianocanuto.controleestoque.dto.ProdutoNomeQuantidadeDTO;
+import com.julianocanuto.controleestoque.repositorios.ProdutoRepository;
+import com.julianocanuto.controleestoque.repositorios.projections.ProdutoNomeQuantidadeProjection;
+
 @SpringBootApplication
-public class ControleestoqueApplication {
+public class ControleestoqueApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleestoqueApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<ProdutoNomeQuantidadeProjection> list = produtoRepository.getNomeQuantidadeProdutos();
+		List<ProdutoNomeQuantidadeDTO> listDto = list.stream().map(x -> new ProdutoNomeQuantidadeDTO(x))
+				.collect(Collectors.toList());
+
+		for (ProdutoNomeQuantidadeDTO obj : listDto) {
+			System.out.println(obj);
+		}
 	}
 
 }
