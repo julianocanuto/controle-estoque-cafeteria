fd1d387866ba2038fa4d2f8f9856e0cfa9674278
Juliano Canuto <julianocanuto@gmail.com>
2021-12-31 00:25:07 -0300

    Adiciona anotação unique no atributo email do User e remove chamada super() no construtor

--- src/main/java/com/julianocanuto/controleestoque/entidades/User.java
+++ src/main/java/com/julianocanuto/controleestoque/entidades/User.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -23,6 +24,8 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 
@@ -34,7 +37,6 @@ public class User implements Serializable {
 	}
 
 	public User(Long id, String name, String email, String password) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
