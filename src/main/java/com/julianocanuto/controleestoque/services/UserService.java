750dd071aa80062c8aaf42d2151d7d90fa376dc2
Juliano Canuto <julianocanuto@gmail.com>
2021-12-31 00:41:11 -0300

    Trata exceções no CRUD do User

--- src/main/java/com/julianocanuto/controleestoque/services/UserService.java
+++ src/main/java/com/julianocanuto/controleestoque/services/UserService.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,13 +21,15 @@ import com.julianocanuto.controleestoque.entidades.Role;
 import com.julianocanuto.controleestoque.entidades.User;
 import com.julianocanuto.controleestoque.repositorios.RoleRepository;
 import com.julianocanuto.controleestoque.repositorios.UserRepository;
+import com.julianocanuto.controleestoque.services.exceptions.DatabaseException;
+import com.julianocanuto.controleestoque.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	UserRepository repository;
 
@@ -47,7 +53,7 @@ public class UserService {
 	public UserDTO findById(Long id) {
 
 		Optional<User> obj = repository.findById(id);
-		User entity = obj.get();
+		User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
 		return new UserDTO(entity);
 	}
 
@@ -61,23 +67,33 @@ public class UserService {
 
 	@Transactional
 	public UserDTO update(Long id, UserDTO dto) {
-		User entity = repository.getById(id);
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
+		try {
+			User entity = repository.getById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new UserDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " não encontrado");
+		}
 
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
 
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
 		entity.setPassword(passwordEncoder.encode(dto.getPassword()));
-		
+
 		entity.getRoles().clear();
 		for (RoleDTO roleDTO : dto.getRoles()) {
 			Role role = roleRepository.getById(roleDTO.getId());
